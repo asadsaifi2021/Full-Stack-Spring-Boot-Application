5fd24f0d9456df40d324c612837c35cd3b89bca5
Asad Saifi <89274067+asadsaifi2021@users.noreply.github.com>
2022-09-07 18:35:47 -0500

    Add files via upload

---
+++ Spring Boot Code/StudentadministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asadsaifi.studentadministration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentadministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
